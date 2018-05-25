a2b6e6f8363f6dadc77123cb67761075d2880072
dla1434 <dla1434@naver.com>
2018-05-25 16:43:24 +0900

    +Add SpringSecurityConfig

--- src/main/java/com/basic/sb/config/SpringSecurityConfig.java
+++ src/main/java/com/basic/sb/config/SpringSecurityConfig.java
@@ -1,8 +1,5 @@
 package com.basic.sb.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -25,8 +22,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers("/**").permitAll()
 			.and()
 			.formLogin()
-	        .loginPage("/login")
-	        .permitAll()
-	        .and();
+			.loginPage("/login")
+			.permitAll()
+			.and();
 	}
 }
