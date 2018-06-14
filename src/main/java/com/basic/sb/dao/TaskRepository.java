926655ba1c28c6ff665eba1691b90171863cf31b
dla1434 <dla1434@naver.com>
2018-06-14 10:50:49 +0900

    modify ignore

--- src/main/java/com/basic/sb/dao/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.basic.sb.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.basic.sb.model.Task;
-
-public interface TaskRepository extends CrudRepository<Task, Integer>{
-
-}
